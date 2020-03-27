c56db542c56b298f0a7a0d23cb04e7af724f1c67
Heisenberg <Persey_98@list.ru>
2020-03-27 22:22:43 +0500

    Homework3

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Homework3
+{
+    public class Program
+    {
+        static void Main (string [] args)
+        {
+            // Console.WriteLine ("ЗАдаЧа 2");
+            // System.Console.Write("Введите вашу сумму:");
+            // double Summa = double.Parse (Console.ReadLine ());
+
+            // if (Summa > 500 && Summa < 1000) 
+            // {
+            //     Summa -= (Summa * 3 / 100);
+            //     System.Console.WriteLine($"3 % Скидки = {Summa}");
+            // }
+            // else if (Summa > 1000)
+            // {
+            //     Summa -= (Summa * 5 / 100);
+            //     System.Console.WriteLine($"5 % Скидки = {Summa}");
+            // }
+
+            // System.Console.WriteLine($"Сумма покупки:{Summa}");
+            // Console.ReadKey ();
+
+            // Голова вообще не варит(((
+
+
+
+
+
+            // System.Console.WriteLine("заДАча 3");
+            // System.Console.WriteLine("Enter 4 numbers :");
+            // double a = double.Parse (Console.ReadLine ());
+            // double b = double.Parse (Console.ReadLine ());
+            // double c = double.Parse (Console.ReadLine ());
+            // double d = double.Parse (Console.ReadLine ());
+
+            // if (a == b && b == c && c == d) System.Console.WriteLine($"mult = {a * b * c * d}");
+            // else if (a <= b && b <= c && c <= d) System.Console.WriteLine("Числа расположены по возрастанию");
+            // else 
+            // {  
+            //     if (a < b && a < c && a < d)
+            //     {
+            //         System.Console.WriteLine($"{a} - min");
+            //     }
+            //     if (b < a && b < c && b < d)
+            //     {
+            //         System.Console.WriteLine($"{b} - min");
+            //     }
+            //     if (c < a && c < b && c < d)
+            //     {
+            //         System.Console.WriteLine($"{c} - min");
+            //     }
+            //     if (d < a && d < b && d < c)
+            //     {
+            //         System.Console.WriteLine($"{d} - min");
+            //     }
+            // }
+            
+            // Console.ReadKey();
+
+            //Не мог додуматься как расположены числа 
+            //Еле - еле додумался как вывести минимальное значение
+
+
+
+            // System.Console.WriteLine("ЗАДАНИЕ 4");
+            // System.Console.WriteLine("Введите а,b,c:");
+            // double a = double.Parse (Console.ReadLine());
+            // double b = double.Parse (Console.ReadLine());
+            // double c = double.Parse (Console.ReadLine());
+            // double t = 0;
+
+            // if (a >= b && b >= c) System.Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+            // else if (a <= b && b <= c && a <= c) 
+            // {
+            //     t = a; a = c; c = t;
+            //     System.Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+            // }
+            // else if (b <= a && b <= c && a <= c) 
+            // {
+            //     t = c; c = b; b = a; a = t;
+            //     System.Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+            // }
+            // else if (a <= b && c <= a) 
+            // {
+            //     t = a; a = b; b = t;
+            //     System.Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+            // }
+            // else if (c <= b && a <= c)
+            // {
+            //     t = a; a = b; b = c; c = t;
+            //     System.Console.WriteLine($"a = {a}; b ={b}; c = {c}");
+            // }
+            
+            // Подглядел с интеренета.Запутался в условиях.Сам не справился.
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
